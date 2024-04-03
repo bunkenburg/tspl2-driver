d8171600c42205c1b9a6eca011d704b8bc2eddb7
Alexander Bunkenburg <alexander.bunkenburg@chartboost.com>
2024-04-03 09:58:51 +0200

    Don't rename packages.

---
+++ src/main/java/org/fintrace/core/drivers/tspl/commands/label/Circle.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 ${project.organization.name}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fintrace.core.drivers.tspl.commands.label;
+
+import org.fintrace.core.drivers.tspl.exceptions.LabelParserException;
+import lombok.Builder;
+import lombok.Data;
+import org.fintrace.core.drivers.tspl.commands.TSPLCommand;
+
+import static org.fintrace.core.drivers.tspl.DriverConstants.*;
+import static org.fintrace.core.drivers.tspl.commands.label.LabelFormatCommand.CIRCLE;
+
+/**
+ * This command draws a circle on the label.<br>
+ * <p>
+ * <b>Syntax</b><br>
+ * CIRCLE X_start,Y_start,diameter,thickness<br>
+ * X_start: Specify x-coordinate of upper left corner (in dots)<br>
+ * Y_start: Specify y-coordinate of upper left corner (in dots)<br>
+ * diameter: Specify the diameter of the circle (in dots)<br>
+ * thickness: Thickness of the circle (in dots)<br>
+ *
+ * @author Venkaiah Chowdary Koneru
+ */
+@Data
+@Builder
+public class Circle implements TSPLCommand {
+
+    /**
+     * x-coordinate of upper left corner (in dots)
+     */
+    private Integer xStart;
+
+    /**
+     * y-coordinate of upper left corner (in dots)
+     */
+    private Integer yStart;
+
+    /**
+     * the diameter of the circle (in dots)
+     */
+    private Integer diameter;
+
+    /**
+     * Thickness of the circle (in dots)
+     */
+    private Integer thickness;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCommand() {
+        if (xStart == null || yStart == null) {
+            throw new LabelParserException("CIRCLE: x and y positions are required");
+        }
+
+        if (diameter == null) {
+            throw new LabelParserException("CIRCLE: please specify diameter");
+        }
+
+        if (thickness == null) {
+            throw new LabelParserException("CIRCLE: please specify thickness");
+        }
+
+        StringBuilder commandBuilder = new StringBuilder(CIRCLE.name());
+        commandBuilder.append(EMPTY_SPACE)
+                .append(xStart).append(COMMA)
+                .append(yStart).append(COMMA)
+                .append(diameter).append(COMMA)
+                .append(thickness)
+                .append(LF);
+
+        return commandBuilder.toString();
+    }
+}
---
+++ src/main/java/org/fintrace/core/drivers/tspl/commands/system/FeedOffset.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 ${project.organization.name}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fintrace.core.drivers.tspl.commands.system;
+
+import org.fintrace.core.drivers.tspl.exceptions.LabelParserException;
+import lombok.Builder;
+import lombok.Data;
+import org.fintrace.core.drivers.tspl.commands.TSPLCommand;
+
+import static org.fintrace.core.drivers.tspl.DriverConstants.*;
+import static org.fintrace.core.drivers.tspl.commands.label.TSPLLabelUtils.hasFloatDecimals;
+
+/**
+ * This command defines the selective, extra label feeding length
+ * each form feed takes, which, especially in peel-off mode and cutter
+ * mode, is used to adjust label stop position, so as for label to
+ * register at proper places for the intended purposes. The printer back
+ * tracks the extra feeding length before the next run of printing.
+ *
+ * @author Venkaiah Chowdary Koneru
+ */
+@Data
+@Builder
+public class FeedOffset implements TSPLCommand {
+    /**
+     * The offset distance (mm).<br>
+     * <b>CAUTION: </b>Impropriety offset value may cause paper jam.
+     */
+    private Float offsetDistance;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCommand() {
+        if (offsetDistance == null) {
+            throw new LabelParserException("ParseException OFFSET Command: offset can't be empty");
+        }
+
+        StringBuilder commandBuilder = new StringBuilder(SystemCommand.OFFSET.name());
+        commandBuilder.append(EMPTY_SPACE);
+
+        if (!hasFloatDecimals(offsetDistance)) {
+            commandBuilder.append(offsetDistance.intValue());
+        } else {
+            commandBuilder.append(offsetDistance);
+        }
+
+        commandBuilder.append(EMPTY_SPACE).append(UNIT_MM)
+                .append(LF);
+
+        return commandBuilder.toString();
+    }
+}
